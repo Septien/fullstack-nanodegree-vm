f3b6e1698938d859884fb18e33ed5535a2b20283
Jose Septien <jaseptienh@gmail.com>
2016-10-27 11:46:16 -0500

    Add class for shelters. More information to add.

--- vagrant/puppy_shelters/database_setup.py
+++ vagrant/puppy_shelters/database_setup.py
@@ -6,6 +6,20 @@ from sqlalchemy.ext.declarative_base import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+#Create class for the corresponding tables to use
+class Shelter(Base):
+    __table_name__ = 'shelter'
+    #
+    name = Column(String(80), nullable = False)
+    #
+    address = Column(String(80), nullable = False)
+    #
+    city = Column(String(80), nullable = False)
+    #
+    state = Column(String(80))
+    #
+
+
 #Create engine for database contection
 engine = create_engine('sqlite:///puppies.db')
 #Add created class, as tables, to the database
