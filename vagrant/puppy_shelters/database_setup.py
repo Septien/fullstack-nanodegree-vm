e414e2c4388ecc8ff59377751cb96370ba205472
Jose Septien <jaseptienh@gmail.com>
2016-10-27 11:34:42 -0500

    Add configuration commands to database setup.

--- vagrant/puppy_shelters/database_setup.py
+++ vagrant/puppy_shelters/database_setup.py
@@ -1,1 +1,12 @@
+#Necesary modules
+#Configuration modules
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative_base import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 
+#Create engine for database contection
+engine = create_engine('sqlite:///puppies.db')
+#Add created class, as tables, to the database
+Base.metadata.create_all(engine)
