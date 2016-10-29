c2add800585eb42db5e8cf1ca0f52106309735a2
Jose Septien <jaseptienh@gmail.com>
2016-10-29 18:21:18 -0500

    Add solution of problem 2.

--- vagrant/puppy_shelters/examples.py
+++ vagrant/puppy_shelters/examples.py
@@ -1,9 +1,9 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, Shelter, Puppy
 from random import randint
-import datetime
+import datetime as dt
 import random
 
 engine = create_engine('sqlite:///puppyshelter.db')
@@ -13,6 +13,14 @@ session = DBSession()
 
 #1. Query all puppies and return the result in ascending alphabetical order
 query = session.query(Puppy).order_by(Puppy.name)
+
+#for q in query:
+#    print q.name, q.dateOfBirth, q.gender, q.weight
+
+#2. Query all the objects that are less than 6 months old organized by the youngest first
+sixmonths = dt.date.today() - dt.timedelta(weeks = 26)
+
+query = session.query(Puppy).filter(Puppy.dateOfBirth >= sixmonths).order_by(desc(Puppy.dateOfBirth))
 
 for q in query:
     print q.name, q.dateOfBirth, q.gender, q.weight
