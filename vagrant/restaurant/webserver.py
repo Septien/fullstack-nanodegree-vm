df5be3abd0e65f9937c2ae0283f127be4d694c51
Jose Septien <jaseptienh@gmail.com>
2016-10-29 13:28:24 -0500

    Add basics commands to create server.

--- vagrant/restaurant/webserver.py
+++ vagrant/restaurant/webserver.py
@@ -1,1 +1,15 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+def main():
+    try:
+        port = 8080
+        #Host as empty string
+        server = HTTPServer (('', port), webserverHandler)
+
+    #Handle ctrl+c interruption
+    except KeyboardInterrupt:
+
+
+
+if __name__ == '__main__':
+    main()
